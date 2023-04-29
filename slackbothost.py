e510a99a7a2c34d70cd7b1cca8e3df6f999b3167
Michael Corban <78115284+MichaelCorban@users.noreply.github.com>
2023-04-28 20:25:43 -0400

    Update slackbothost.py

--- slackbothost.py
+++ slackbothost.py
@@ -15,14 +15,11 @@ load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
-#client.chat_postMessage(channel='#general', text="Hello")
 
-#app = Flask(__name__)
 app = App(
     token=os.environ.get("SLACK_TOKEN"),
     signing_secret=os.environ.get("SIGNING_SECRET")
 )
-#slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
 
 @app.event("message")
 def event_test(message, body, say, logger):
@@ -39,24 +36,6 @@ handler = SlackRequestHandler(app)
 @flask_app.route("/slack/events", methods = ["POST"])
 def slack_events():
     return handler.handle(request)
-
-'''
-@slack_event_adapter.on('message')
-def message(payload):
-    #print(payload)
-    print("hi")
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    type = event.get('type')
-
-    if BOT_ID != user_id:
-        inpt = text
-        #client.chat_postMessage(channel=channel_id, text=chatbotpy311.query(inpt))
-        response = chatbotpy311.test(inpt)
-        client.chat_postMessage(channel=channel_id, text=response)
-'''
         
 if __name__ == "__main__":
     flask_app.run(debug=True)
